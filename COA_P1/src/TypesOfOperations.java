980ccbde0d8c7c03418c705e064f05feb6271024
Chetan <cgajare1@binghamton.edu>
2016-11-19 01:32:51 -0500

    final refactore

--- COA_P1/src/TypesOfOperations.java
+++ COA_P1/src/TypesOfOperations.java
@@ -1,25 +1,24 @@
 /**
- * @author Chetan
  *
+ *This class is used to declare the variables for different types of operations
+ *<p>
+ *{@code 	public static final String ADD = "ADD";}
  */
-public final class TypesOfOperations {
-	
+public final class TypesOfOperations
+{
+
 	public static final String ADD = "ADD";
 	public static final String SUB = "SUB";
 	public static final String MUL = "MUL";
+	public static final String MOVC = "MOVC";
 	public static final String AND = "AND";
 	public static final String OR = "OR";
 	public static final String EXOR = "EX-OR";
-	public static final String MOVC = "MOVC";
-	public static final String MOV = "MOV";
-
 	public static final String LOAD = "LOAD";
 	public static final String STORE = "STORE";
-
 	public static final String BZ = "BZ";
 	public static final String BNZ = "BNZ";
 	public static final String JUMP = "JUMP";
 	public static final String BAL = "BAL";
 	public static final String HALT = "HALT";
-	
 }
