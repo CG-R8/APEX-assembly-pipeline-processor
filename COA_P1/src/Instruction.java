356536baa9dd327ff4ca90f6a93008001cf79ba0
Chetan <cgajare1@binghamton.edu>
2016-11-18 23:32:33 -0500

    refactor 3

--- COA_P1/src/Instruction.java
+++ COA_P1/src/Instruction.java
@@ -1,6 +1,5 @@
 
 /**
- * @author Chetan
  *KeyValue<String, Integer> src1;
  */
 public class Instruction {
@@ -39,63 +38,42 @@ public class Instruction {
 		this.isNOP = false;
 	}
 
-	// Returns Instruction's Operation type (ADD/SUB/MUL/AND/OR/EXOR/MOV/LOAD/STORE .. etc)
+	public String getContent(){
+		return content;
+	}	
 	public String getOperation(){
 		return this.operation;
 	}
-
-	// Set value in Instruction's Destination field
-	public void setDestination(Integer value){
-		this.destination.setValue(value);
-	}
-
-	// Returns Instruction's Destination <key,value> field (Example: <R1, 100>)
-	public KeyValue<String, Integer> getDestination(){
-		return this.destination;	
-	}	
-
-	// Set value in Instruction's Source1 field
-	public void setSrc1(Integer value){
-		this.src1.setValue(value);
-	}
-
-	// Returns Instruction's Source1 <key,value> field (Example: <R1, 100>)
-	public KeyValue<String, Integer> getSrc1(){
-		return this.src1;	
-	}
-
-	// Set value in Instruction's Source2 field
-	public void setSrc2(Integer value){
-		this.src2.setValue(value);
-	}
-
-	// Returns Instruction's Source2 <key,value> field (Example: <R1, 100>)
-	public KeyValue<String, Integer> getSrc2(){
-		return this.src2;	
-	}
-
-	// Returns Instruction's literal value
-	public Integer getLiteral(){
-		return this.literal;
-	}
-
-	// Sets memory address for Store Instruction
-	public void setMemoryAddress(Integer value){
-		this.memAddress =  value;
-	}
-
-	// Returns memory address of Instruction if any
 	public Integer getMemoryAddress(){
 		return this.memAddress;
 	}
-	
-	// Returns NOP (in case of halt instruction)
+	public KeyValue<String, Integer> getSrc1(){
+		return this.src1;	
+	}
+	public KeyValue<String, Integer> getSrc2(){
+		return this.src2;	
+	}
+	public Integer getLiteral(){
+		return this.literal;
+	}
+	public KeyValue<String, Integer> getDestination(){
+		return this.destination;	
+	}
+	public void setSrc1(Integer value){
+		this.src1.setValue(value);
+	}
+	public void setSrc2(Integer value){
+		this.src2.setValue(value);
+	}
+	public void setDestination(Integer value){
+		this.destination.setValue(value);
+	}
+	public void setMemoryAddress(Integer value){
+		this.memAddress =  value;
+	}	
 	public boolean isNOP(){
 		return this.isNOP;
 	}
 
-	//Returns Instruction string(Example: MOVC R0 1)
-	public String getContent(){
-		return content;
-	}
+
 }
